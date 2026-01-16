20ede99358e911e28973f9b88b912b42749aba9c
Uros Karic <hi@uros.dev>
2026-01-16 19:19:12 +0100

    Remove maintainer until PR merged

--- package.nix
+++ package.nix
@@ -64,7 +64,7 @@ stdenvNoCC.mkDerivation {
     homepage = "https://github.com/tw93/Mole";
     changelog = "https://github.com/tw93/Mole/releases/tag/V${version}";
     license = lib.licenses.mit;
-    maintainers = [ lib.maintainers.djedlajn ];
+    maintainers = [ ];
     platforms = [
       "aarch64-darwin"
       "x86_64-darwin"
